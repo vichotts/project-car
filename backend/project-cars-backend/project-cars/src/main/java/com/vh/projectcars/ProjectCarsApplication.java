562ee448ef2645f1792ece2e4fd037282c333881
Victor Hotts <hotts_victor@hotmail.com>
2019-08-16 11:23:10 -0300

    Testando

---
+++ backend/project-cars-backend/project-cars/src/main/java/com/vh/projectcars/ProjectCarsApplication.java
@@ -0,0 +1,13 @@
+package com.vh.projectcars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectCarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCarsApplication.class, args);
+	}
+
+}
